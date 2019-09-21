abe2125ab751c79392da44b1aa29263212e11f28
Anime Sex Storm (ASS) <puppy0caminminecraft@gmail.com>
2019-09-21 19:41:08 +1000

    change a mistaken import

--- index.mjs
+++ index.mjs
@@ -1,3 +1,3 @@
-import ChatWars3GameTime from "./ChatWars3GameTime.js";
-import InternationalGameTime from "./InternationalGameTime.js";
+import ChatWars3GameTime from "./ChatWars3GameTime.mjs";
+import InternationalGameTime from "./InternationalGameTime.mjs";
 export { ChatWars3GameTime, InternationalGameTime, };
